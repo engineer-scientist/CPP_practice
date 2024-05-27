82eaf100132c4bae0acaf9391b9bc588c02da77c
Sarthak Sharma <54902964+engineer-scientist@users.noreply.github.com>
2024-05-27 11:04:10 -0600

    Add files via upload

---
+++ p17_average_of_array_elements.cpp
@@ -0,0 +1,34 @@
+/*
+Q. 17. Write a C++ program to calculate the average of all the elements present in an array.
+For example: 
+arr[] = {10, 22, 45, 11};
+Output: Average = 22
+*/
+
+# include <iostream>
+
+int main() {
+	
+	int n;
+	std :: cout << "Enter the number of elements in the array. Please enter a positive integer: ";
+	std :: cin >> n;
+	while (n < 1) {
+		std :: cout << "The number of array elements can not be zero or negative. Please enter a positive integer: ";
+		std :: cin >> n;
+	}
+	
+	double arr [n], sum = 0, avg;
+	
+	for (int i = 0; i < n; i ++) {
+		std :: cout << "Enter the array element at index " << i << ": ";
+		std :: cin >> arr[i];
+		sum = sum + arr[i];
+	}
+	
+	avg = sum / n;
+	
+	std :: cout << "The average of all elements in the array is " << avg << ". \n";
+	
+	return 0;
+	
+}
